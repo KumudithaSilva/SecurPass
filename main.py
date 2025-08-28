4a0e0652b1be0f44b7ea311d027ba5888a5b79bf
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-08-29 02:13:33 +0530

    add empty variable

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from tkinter import Tk
 from securpass.resources import ResourceLoader
 from securpass.ui import SecureUI
 
+
 def main():
     root = Tk()
     loader = ResourceLoader()
-
     images = {
         "main_logo": loader.load_image("securepass.png"),
         "main_icon": loader.load_icon("securepass.ico")
     }
-    SecureUI(root, images)
+    app = SecureUI(root, images)
     root.mainloop()
 
 
