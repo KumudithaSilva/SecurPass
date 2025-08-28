f43ed13ec10e7424bc8b4fe3b098efa58c6cce10
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-08-29 01:26:52 +0530

    Remove main screen UI part

--- main.py
+++ main.py
@@ -5,22 +5,15 @@ from securpass.ui import SecureUI
 
 def main():
     root = Tk()
-    root.title("SecurePass")
-    root.geometry("250x250")
-    root.config(padx=20, pady=20, bg="#fdfdfd")
-    root.resizable(False, False)
-    # root.iconbitmap("images/pasword-manager.ico")
-
     loader = ResourceLoader()
     images = {
-        "main_logo": loader.load_image("securepass.png")
+        "main_logo": loader.load_image("securepass.png"),
+        "main_icon": loader.load_icon("securepass.ico")
     }
-
     app = SecureUI(root, images)
     root.mainloop()
 
 
 if __name__ == "__main__":
     main()
-
 
