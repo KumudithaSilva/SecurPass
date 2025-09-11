ed1abed61ca09b95e2c51e1185dd30b1c4f0d501
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-09-12 04:28:30 +0530

    Add config file

---
+++ securpass/config.py
@@ -0,0 +1,17 @@
+class Config:
+    """
+    Configuration for SecurPass
+
+    Attributes:
+        EMAIL: Default email
+
+    """
+
+    def __init__(self, email="kumudithasilva66@gmail.com"):
+        """
+        Initialize Config with optional custom details.
+
+        Args:
+            email: Default user email
+        """
+        self.EMAIL = email
