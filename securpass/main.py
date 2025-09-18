296e75e927e4b1d123db0f11702515a24a39dad8
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-09-18 21:47:49 +0530

    Add messagebox

--- securpass/main.py
+++ securpass/main.py
@@ -4,6 +4,7 @@ from securpass.SecurePassController import SecurePassController
 from securpass.config import Config
 from securpass.data_retrieval import Data_Retrieval
 from securpass.data_save import Data_Saver
+from securpass.messagebox import Messagebox
 from securpass.resources import ResourceLoader
 from securpass.ui import SecureUI
 
@@ -16,16 +17,15 @@ def main():
         "main_icon": loader.get_resource_path("securepass.ico"),
     }
 
-    config = Config()
     ui = SecureUI(root, images)
-
+    config = Config()
     saver = Data_Saver()
-    retriever = Data_Retrieval(ui)
+    messagebox = Messagebox()
+    retriever = Data_Retrieval(ui, config, messagebox)
 
-    SecurePassController(ui, config, saver, retriever)
+    SecurePassController(ui, config, saver, retriever, messagebox)
 
     root.mainloop()
-
     saver.close()
 
 
