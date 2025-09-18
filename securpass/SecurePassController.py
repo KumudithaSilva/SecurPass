ad17359aa2f3f2fbdac5cebc9fcfae88ccabb25b
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-09-18 21:47:30 +0530

    Inject messagebox and save data condition

--- securpass/SecurePassController.py
+++ securpass/SecurePassController.py
@@ -1,3 +1,4 @@
+from securpass.messagebox import Messagebox
 from securpass.config import Config
 from securpass.data_retrieval import Data_Retrieval
 from securpass.data_save import Data_Saver
@@ -13,10 +14,16 @@ class SecurePassController:
         config: Config instance containing default values.
         saver: DataSaver instance for saving retrieved data.
         retriever: DataRetrieval instance to fetch data from the UI.
+        message (MessageBox): Instance to display confirmations and info.
     """
 
     def __init__(
-        self, ui: SecureUI, config: Config, saver: Data_Saver, retriever: Data_Retrieval
+        self,
+        ui: SecureUI,
+        config: Config,
+        saver: Data_Saver,
+        retriever: Data_Retrieval,
+        message: Messagebox,
     ):
         """
         Initialize the SecurePassController.
@@ -26,11 +33,13 @@ class SecurePassController:
             config: Config instance containing default values.
             saver: DataSaver instance to save data.
             retriever: DataRetrieval instance to retrieve data from UI.
+            message (MessageBox): Instance to display confirmations and info.
         """
         self.ui = ui
         self.config = config
         self.saver = saver
         self.retriever: Data_Retrieval = retriever
+        self.message = message
 
         # Set default email in UI
         self.ui.set_email(self.config.EMAIL)
@@ -41,4 +50,10 @@ class SecurePassController:
     def add_entry(self):
         """Retrieve data from UI and save it."""
         data = self.retriever.ui_data_retrieval()
-        self.saver.data_save(data)
+        website = data.get("website", "Unknown")
+
+        if self.message.ask_confirmation(self.config.MSG_CONFIRM_SAVE.format(website)):
+            self.saver.data_save(data)
+            self.message.show_info(self.config.MSG_ENTRY_SAVED)
+        else:
+            self.message.show_info(self.config.MSG_ENTRY_CANCELLED)
