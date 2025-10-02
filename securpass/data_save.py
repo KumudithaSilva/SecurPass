ed0642009fc04fd1f61a65f34e257f331584d51d
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-10-02 23:43:20 +0530

    Add JSON file formate

--- securpass/data_save.py
+++ securpass/data_save.py
@@ -1,28 +1,26 @@
+import  json
+import os.path
+
 class Data_Saver:
+
     """
-    Save the entry values to CSV .
+    Save website, email, and password entries to a JSON file.
 
     Attributes:
-        filename (str): Path to the CSV file where data will be stored.
-        file: Open file object.
-        writer: DictWriter object.
-
+        filename (str): Path to the JSON file where data will be stored.
     """
 
-    HEADERS = ["website", "email", "password"]
-
-    def __init__(self, filename: str = "data.csv"):
+    def __init__(self, filename = "data.json"):
         """
         Initialize the Data_Saver.
 
         Args:
-            filename (str): Path to the CSV file where data will be stored.
+            filename (str): Path to the JSON file where data will be stored.
         """
         self.filename = filename
-        self.file = open(self.filename, mode="a", newline="", encoding="utf-8")
-
-        if self.file.tell() == 0:
-            self.file.write(" | ".join(self.HEADERS) + "\n")
+        if not os.path.isfile(self.filename):
+            with open(self.filename, "w", encoding="utf-8") as file:
+                json.dump([], file)
 
     # --------------------------
     # Data Save  Methods
@@ -30,16 +28,30 @@ class Data_Saver:
 
     def data_save(self, data: dict):
         """
-        Save website, email, and password to the CSV.
+        Save website, email, and password to the JSON file.
 
         Args:
             data (dict): Dictionary containing 'website', 'email', and 'password'.
-
         """
-        row = " | ".join(data[h] for h in self.HEADERS)
-        self.file.write(row + "\n")
-        self.file.flush()
+        with open(self.filename, "r+", encoding="utf-8") as file:
+            existing_data = json.load(file)
+            existing_data.append(data)
+            file.seek(0)
+            json.dump(existing_data, file, indent=4)
+            file.truncate()
 
-    def close(self):
-        """Close the CSV file properly."""
-        self.file.close()
+    # --------------------------
+    # Data Read  Methods
+    # --------------------------
+
+    def data_read(self):
+        """
+        Retrieve website, email, and password from the JSON file.
+
+        Returns:
+            dict: Dictionary containing 'website', 'email', and 'password' keys.
+        """
+        with open(self.filename, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            print(data)
+            return data
