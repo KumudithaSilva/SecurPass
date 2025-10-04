033b468f22f571cd276e92f3ca7e7de4557c370c
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-10-04 05:33:05 +0530

    Wire search button calback

--- securpass/SecurePassController.py
+++ securpass/SecurePassController.py
@@ -1,3 +1,4 @@
+from securpass.errors import PasswordTooShortError
 from securpass.messagebox import Messagebox
 from securpass.config import Config
 from securpass.data_retrieval import Data_Retrieval
@@ -54,6 +55,7 @@ class SecurePassController:
         # Wire the Generate Password and Add buttons to controller method
         self.ui.set_password_callback(self.generate_password)
         self.ui.set_add_callback(self.add_entry)
+        self.ui.set_search_callback(self.search_entry)
 
     def generate_password(self):
         """
@@ -73,7 +75,25 @@ class SecurePassController:
         website = data.get("website", "Unknown")
 
         if self.message.ask_confirmation(self.config.MSG_CONFIRM_SAVE.format(website)):
-            self.saver.data_save(data)
-            self.message.show_info(self.config.MSG_ENTRY_SAVED)
+            try:
+                self.saver.data_save(data)
+                self.message.show_info(self.config.MSG_ENTRY_SAVED)
+            except PasswordTooShortError:
+                raise PasswordTooShortError(self.config.MSG_PASSWORD_TOO_SHORT)
         else:
             self.message.show_info(self.config.MSG_ENTRY_CANCELLED)
+
+    def search_entry(self):
+        """
+        Retrieve website's name data from UI entries, get website's data,
+        and display all information.
+        """
+        website_name = self.retriever.get_website_name()
+        full_data = self.saver.data_read()
+
+        entry = full_data.get(website_name)
+        if entry:
+            self.ui.set_email(entry['email'])
+            self.ui.set_password(entry['password'])
+        else:
+            self.message.show_info(self.config.NO_ENTRY.format(website_name))
